c339fc6407e480988796e0fddf0db73756c91607
J-Speights <jspeights@aegisfoundry.com>
2024-04-05 19:53:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ from schema_formatter.schema_management import (
 
 
 def main() -> None:
+    # TODO: Alter database query: this goes to the cloud db.
     exit_code = backup_postgresql_schema()
     if not exit_code:
         exit_code = convert_schema_from_pg_to_sqlite()
     if not exit_code:
         exit_code = create_database()
+    # TODO: Upload to S3 bucket (New sqlite db).
+    # TODO: Notify dev of change. (Email, Teams.)
     if exit_code:
         sys.exit(exit_code)
 
+    # TODO: write base64 query to the ref_local_db_updates table.
     sys.exit(0)
 
 
